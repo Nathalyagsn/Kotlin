c33a6cf06690d3a66a03c62952033ab84527851a
Nathalya Silva <nathalya.gsn@gmail.com>
2025-07-23 16:50:56 -0300

    variaveis

--- app/src/main/java/com/example/aulasintrodutorias/Variaveis.kt
+++ app/src/main/java/com/example/aulasintrodutorias/Variaveis.kt
@@ -15,10 +15,23 @@
     //println("I'm a short person, i'm $height tall")
 //}
 
+//fun main() {
+    //val name = "Nathalya"
+    //val lasName = "Nascimento"
+    //var age = 25
+    //val height = 1.48
+    //println("Hello, i'm $name $lasName. I have $age years old and i'm a short person, i'm $height tall")
+//}
+
+//fun main() {
+    //var pontuacao = 100
+    //println("Pontuação inicial $pontuacao")
+    //var pontuacao = 150
+    //println("Nova pontuação $pontuacao")
+//}
+
+const val max_tentativas = 3
 fun main() {
-    val name = "Nathalya"
-    val lasName = "Nascimento"
-    var age = 25
-    val height = 1.48
-    println("Hello, i'm $name $lasName. I have $age years old and i'm a short person, i'm $height tall")
+    val mensagemInicial = "Você tem $max_tentativas tentativas"
+    println(mensagemInicial)
 }
