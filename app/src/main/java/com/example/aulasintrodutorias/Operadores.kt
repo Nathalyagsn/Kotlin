aedd34e0c2f4ded71e20eda985d5dedefaa7d5d9
Nathalya Silva <nathalya.gsn@gmail.com>
2025-07-24 15:21:08 -0300

    Operadores

---
+++ app/src/main/java/com/example/aulasintrodutorias/Operadores.kt
@@ -0,0 +1,75 @@
+//fun main() {
+    //val nota1 = 7.5F
+    //val nota2 = 5
+    //val nota3 = 4.5F
+
+    //val somaDasNotas = nota1 + nota2 + nota3
+    //val mediaDasNotas = somaDasNotas / 3
+
+    //println("Média das notas: $mediaDasNotas")
+
+//}
+
+//fun main() {
+    //val idade = 25
+    //val diasDoAno = 365
+    //val diasDeVida = 25 * 365
+    //println("Você viveu aproximadamente $diasDeVida dias")
+//}
+
+//fun main() {
+    //val totaldeItens = 124
+    //val caixas = 5
+    //val divisaoPorCaixa = totaldeItens /caixas
+    //val itensSobrando = totaldeItens % caixas
+    //println("Itens por caixa: $divisaoPorCaixa")
+    //println("Itens sobrando: $itensSobrando")
+//}
+
+//fun main() {
+    //var pontuacao = 0
+    //pontuacao += 10
+    //pontuacao -=3
+    //pontuacao *= 2
+    //println(pontuacao)
+//}
+
+//fun main() {
+    //var totalCarrinho = 55.0F
+    //totalCarrinho += 12.75F
+    //totalCarrinho -=5.00F
+    //println("Nota Físcal: $totalCarrinho")
+//}
+
+//fun main() {
+    //val idade = 25
+    //print(idade > 18)
+//}
+
+//fun main() {
+  //val numeroA = 100
+  //val numeroB = 101
+    //println(numeroA == numeroB) //false
+    //println(numeroA !=numeroB)  //true
+    //println(numeroA > numeroB)  //false
+    //println(numeroA < numeroB)  //true
+//}
+
+//fun main() {
+    //val mediaFinal = 8.0
+    //val frequencia = 80
+    //print(mediaFinal>=6.0 && frequencia >=75)
+//}
+
+//fun main() {
+    //val estudante = true
+    //val idoso = false
+    //val descontoAplicado = estudante || idoso
+    //println(descontoAplicado)
+
+//}
+
+//fun main() {
+    //val estaChovendo = false
+    //println(!estaChovendo)
+//}
