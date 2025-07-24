a3571df2881b43ebf43e2130306893ac7e96034f
Nathalya Silva <nathalya.gsn@gmail.com>
2025-07-24 11:20:40 -0300

    Tipo de dados

---
+++ app/src/main/java/com/example/aulasintrodutorias/TipoDados.kt
@@ -0,0 +1,58 @@
+//fun main() {
+    //val maxByte : Byte = Byte.MAX_VALUE
+    //val minByte : Byte = Byte.MIN_VALUE
+    //println("Valor máximo $maxByte, Valor mínimo: $minByte")
+//}
+
+//fun main() {
+    //val number1 = 10
+    //val number2 = 4.5F
+
+    //val adicao = number1 + number2
+    //val multiplicacao = number1 * number2
+    //val divisao = number1 / number2
+    //val subtracao = number1 - number2
+
+    //println(adicao)
+    //println(multiplicacao)
+    //println(divisao)
+    //println(subtracao)
+//}
+
+//fun main() {
+    //val diaDeChuva = true
+    //val tenhoSombrinha = false
+
+    //val possoSair = diaDeChuva && !tenhoSombrinha //Se o dia está chovendo e eu tenho sombrinha eu posso air
+    //println("Posso sair? $possoSair")
+
+    //val naoPossoSair = diaDeChuva && tenhoSombrinha //Se o dia está chovendo e eu não tenho sombrinha não posso sair
+    //println("Posso sair? $naoPossoSair")
+//}
+
+//fun main() {
+    //val letra: Char = 'N'
+    //println("Letra: $letra")
+
+    //val saudacao = "Olá, Nathalya"
+    //val nomePet = "Nico"
+
+    //println("Nome do pet em minusculo ${nomePet.lowercase()}")
+    //println("Contem a letra N: ${nomePet.contains('N')}")
+//}
+
+
+import java.time.Year
+//fun main() {
+    //println("Olá. Qual seu nome?")
+    //val nome = readln()
+
+    //val anoAtual = Year.now().value
+
+    //println("$nome, em que ano você nasceu?")
+    //val nascimento = readln()
+
+    //val idade = anoAtual - nascimento.toInt()
+
+    //println("$nome, você tem $idade anos!")
+//}
