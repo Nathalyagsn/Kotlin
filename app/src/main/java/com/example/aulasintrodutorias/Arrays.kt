8b0ada26754996ee98f0205632de613324bac6d6
Nathalya Silva <nathalya.gsn@gmail.com>
2025-08-04 17:50:34 -0300

    Arrays

---
+++ app/src/main/java/com/example/aulasintrodutorias/Arrays.kt
@@ -0,0 +1,40 @@
+//fun main() {
+    //var numerosInt = arrayOf(2, 5, 9, 5, 8, 3, 2, 5)
+    //print(numerosInt.joinToString())
+//}
+
+//fun main() {
+    //var frutas = arrayOf("Jaca", "Morango", "Cajá", "Maçã", "Uva")
+    //print(frutas.joinToString())
+//}
+
+//fun main() {
+    //val filmes = arrayOf("Summerland", "Little Fish", "Carol", "Marrowbone")
+    //println(filmes[1])
+    //println(filmes.size)
+//}
+
+//fun main() {
+    //val listaDeCompras = arrayOf("Batata", "Creme de leite", "Arroz", "Couve","Queijo")
+    //println("----------Minha Lista----------")
+    //for (c in listaDeCompras) {
+        //println("- $c")
+    //}
+//}
+
+//fun main() {
+    //val notas = doubleArrayOf(7.5, 8.0, 5.5, 3.0)
+    //var soma = 0.0
+
+    //for(c in notas){
+        //soma += c
+
+    //}
+    //val media = soma / notas.size
+    //println("A soma das notas é $soma")
+    //println("A média do aluno é $media")
+//}
+
+fun main() {
+    
+}
